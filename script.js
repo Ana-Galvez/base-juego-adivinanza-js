e929905f9e1a08c02c5fc95eac9270fb439993f4
ana galvez <130255411+Ana-Galvez@users.noreply.github.com>
2024-03-03 22:11:55 -0300

    [FEAT]add if the number is greater or less

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const numeroEntrada = document.getElementById('numeroEntrada');
 const mensaje = document.getElementById('mensaje');
 const numero = document.getElementById('numero');
 
-const numeroAleatorio = Math.trunc(Math.random() * 100);
+const numeroAleatorio = Math.trunc(Math.random() * 100) + 1;
 // numero.textContent = numeroAleatorio;
 
 
@@ -10,14 +10,20 @@ function comprobarNumero() {
   if (numeroEntrada.value == numeroAleatorio) {
     mensaje.textContent = 'FELICIDADES, ACERTASTE EL NÚMERO';
     mensaje.style.color = 'green';
+    numeroEntrada.disabled = true;
   } else if (numeroEntrada.value < 1 || numeroEntrada.value > 100) {
     mensaje.textContent = 'EL NÚMERO DEBE SER ENTRE 1 Y 100';
     mensaje.style.color = 'blue';
     numeroEntrada.value = '';
     numeroEntrada.focus();
-  } else {
-    mensaje.textContent = 'NO ES EL NÚMERO';
-    mensaje.style.color = 'red';
+  } else if (numeroEntrada.value > numeroAleatorio) {
+    mensaje.textContent = 'El número es menor al escrito por usted';
+    mensaje.style.color = 'purple';
+    numeroEntrada.value = '';
+    numeroEntrada.focus();
+  } else if (numeroEntrada.value < numeroAleatorio) {
+    mensaje.textContent = 'El número es mayor al escrito por usted';
+    mensaje.style.color = 'grey';
     numeroEntrada.value = '';
     numeroEntrada.focus();
   }
